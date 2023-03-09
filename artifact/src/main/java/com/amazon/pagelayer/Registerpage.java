b29dc6ee1d55ffb70070114403edbcfe541d9ac0
mayank-12123 <jjjmayankjharbade@gmail.com>
2023-03-09 20:46:07 +0530

    New feature added

---
+++ artifact/src/main/java/com/amazon/pagelayer/Registerpage.java
@@ -0,0 +1,14 @@
+package com.amazon.pagelayer;
+
+import org.testng.annotations.Test;
+
+public class Registerpage {
+	
+	@Test
+
+	public void classs()
+	{
+		System.out.println("mayanknajdfj");
+	}
+	
+}
